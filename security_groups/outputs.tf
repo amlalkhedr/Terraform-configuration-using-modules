bd7f2c669161d1e9696d5efc9548f95ccf5ea4f3
amlalkhedr <111167783+amlalkhedr@users.noreply.github.com>
2024-10-02 02:42:52 +0300

    Delete security_groups directory

--- security_groups/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bastion_sg_id" {
-  value       = aws_security_group.bastion_sg.id
-  description = "The ID of the Bastion security group"
-}
-
-output "app_sg_id" {
-  value       = aws_security_group.app_sg.id
-  description = "The ID of the Application security group"
-}
--- security_groups/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "vpc_id" {
-  description = "The VPC ID to associate security groups with"
-  type        = string
-}
-
